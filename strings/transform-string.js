b731e22b7e4d1b2171c357f10f69f4bb1bf67c8c
AakrshitThakur <thakurraakrshitt@gmail.com>
2025-08-22 18:35:40 +0530

    strings/transform-string.js

---
+++ strings/transform-string.js
@@ -0,0 +1,64 @@
+// Given two strings A and B. Find the minimum number of steps required to transform string A into string B. The only allowed operation for the transformation is selecting a character from string A and inserting it in the beginning of string A.
+
+// Example 1:
+
+// Input:
+// A = "abd"
+// B = "bad"
+// Output: 1
+// Explanation: The conversion can take place in
+// 1 operation: Pick 'b' and place it at the front.
+
+// Example 2:
+
+// Input:
+// A = "GeeksForGeeks"
+// B = "ForGeeksGeeks"
+// Output: 3
+// Explanation: The conversion can take
+// place in 3 operations:
+// Pick 'r' and place it at the front.
+// A = "rGeeksFoGeeks"
+// Pick 'o' and place it at the front.
+// A = "orGeeksFGeeks"
+// Pick 'F' and place it at the front.
+// A = "ForGeeksGeeks"
+// Your Task:
+// You dont need to read input or print anything. Complete the function transform() which takes two strings A and B as input parameters and returns the minimum number of steps required to transform A into B. If transformation is not possible return -1.
+
+// Expected Time Complexity: O(N) where N is max(length of A, length of B)
+// Expected Auxiliary Space: O(1)
+
+// Constraints:
+// 1<= A.length(), B.length() <= 104
+
+function transform(A, B) {
+  let n_A = A.length;
+  let n_B = B.length;
+
+  if (n_A !== n_B) return -1;
+
+  let freqA = {};
+  let freqB = {};
+
+  for (let ch of A) freqA[ch] = (freqA[ch] || 0) + 1;
+  for (let ch of B) freqB[ch] = (freqB[ch] || 0) + 1;
+  for (let key in freqA) {
+    if (freqA[key] !== freqB[key]) return -1;
+  }
+
+  let a = n_A - 1;
+  let b = n_B - 1;
+
+  while (a >= 0 && b >= 0) {
+    if (A[a] === B[b]) {
+      b--;
+    }
+    a--;
+  }
+
+  return b + 1;
+}
+
+console.log(transform("abd", "bad"));
+console.log(transform("GeeksForGeeks", "ForGeeksGeeks"));
